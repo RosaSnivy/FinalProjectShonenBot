5974254444b39cb6da2877b5d3e5a3182f24309d
May Ferin <petrasnivy@gmail.com>
2024-12-04 00:50:28 -0600

    Sorting

--- Pages/Characters/Index.cshtml.cs
+++ Pages/Characters/Index.cshtml.cs
@@ -25,12 +25,50 @@ namespace FinalProjectShonenBot.Pages_Characters
         public int PageSize {get; set;} = 7;
         public int TotalPages {get; set;}
 
+        [BindProperty(SupportsGet = true)]
+        public string CurrentSort {get; set;} = string.Empty;
+
         public async Task OnGetAsync()
         {
+            var query = _context.Characters.Include(c => c.Anime).Select(c => c);
+
+            switch (CurrentSort)
+            {
+                case "first_asc":
+                    query = query.OrderBy(c => c.CharacterName);
+                    break;
+                case "first_desc":
+                    query = query.OrderByDescending(c => c.CharacterName);
+                    break;
+                case "HP_asc":
+                    query = query.OrderBy(c => c.HP);
+                break;
+                case "HP_desc":
+                    query = query.OrderByDescending(c => c.HP);
+                break;
+                case "Strength_asc":
+                    query = query.OrderBy(c => c.Strength);
+                break;
+                case "Strength_desc":
+                    query = query.OrderByDescending(c => c.Strength);
+                break;
+                case "Durability_asc":
+                    query = query.OrderBy(c => c.Durability);
+                break;
+                case "Durability_desc":
+                    query = query.OrderByDescending(c => c.Durability);
+                break;
+                case "Anime_asc":
+                    query = query.OrderBy(c => c.Anime.AnimeName);
+                break;
+                case "Anime_desc":
+                    query = query.OrderByDescending(c => c.Anime.AnimeName);
+                break;
+            }
+
             TotalPages = (int)Math.Ceiling(_context.Characters.Count()/(double)PageSize);
 
-            Character = await _context.Characters.Include(a => a.Anime)
-                .Skip((PageNum - 1) * PageSize).Take(PageSize).ToListAsync();
+            Character = await query.Skip((PageNum - 1) * PageSize).Take(PageSize).ToListAsync();
         }
     }
 }
